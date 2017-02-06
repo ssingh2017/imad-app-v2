429d88abc68068f47dcd45d19d93d2983678fa8b
ssingh2017 <shubhamsingh6596@gmail.com>
2017-02-06 23:29:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('article-one',function(req ,res){
-   res.send("Article one is requested") ;
+app.get('/article-one',function(req ,res){
+   res.send('Article one is requested') ;
 });
-app.get('article-two',function(req ,res){
-   res.send("Article 2 is requested") ;
+app.get('/article-two',function(req ,res){
+   res.send('Article 2 is requested') ;
 });
 
 app.get('/ui/style.css', function (req, res) {
